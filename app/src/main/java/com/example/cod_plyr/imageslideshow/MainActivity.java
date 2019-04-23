07ed209c33ba9ac75155e57b312d97a7c70f3527
Ravindra Sharma <rksharmait13@gmail.com>
2019-04-23 22:49:28 +0530

    first checkin

---
+++ app/src/main/java/com/example/cod_plyr/imageslideshow/MainActivity.java
@@ -0,0 +1,62 @@
+package com.example.cod_plyr.imageslideshow;
+
+import android.content.Context;
+import android.support.v4.view.PagerAdapter;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
+class CustomPagerAdapter extends PagerAdapter {
+    int[] mResources = {
+            R.drawable.first,
+            R.drawable.second,
+            R.drawable.third
+
+    };
+    Context mContext;
+    LayoutInflater mLayoutInflater;
+
+    public CustomPagerAdapter(Context context) {
+        mContext = context;
+        mLayoutInflater = (LayoutInflater) mContext.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+    }
+
+    @Override
+    public int getCount() {
+        return mResources.length;
+    }
+
+    @Override
+    public boolean isViewFromObject(View view, Object object) {
+        return view == ((LinearLayout) object);
+    }
+
+    @Override
+    public Object instantiateItem(ViewGroup container, int position) {
+        View itemView = mLayoutInflater.inflate(R.layout.pager_item, container, false);
+
+        ImageView imageView = (ImageView) itemView.findViewById(R.id.image);
+        imageView.setImageResource(mResources[position]);
+
+        container.addView(itemView);
+
+        return itemView;
+    }
+
+    @Override
+    public void destroyItem(ViewGroup container, int position, Object object) {
+        container.removeView((LinearLayout) object);
+    }
+}
